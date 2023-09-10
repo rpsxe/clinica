115b57013e60788518c23e37a45c644d353ad158
Richard <richardpereira@sou.urcamp.edu.br>
2023-09-10 14:40:39 -0300

    conexao

---
+++ conecta/config.php
@@ -0,0 +1,11 @@
+<?php 
+
+// Configurações do banco de dados
+
+$host='localhost';
+$db= 'clinica';
+$charset= 'UTF-8';
+$user='root'; 
+$pass = '';
+
+ ?>
--- reg-consulta.php
+++ reg-consulta.php
@@ -1,69 +1,62 @@
-<?php
-ob_start();
-session_start();
-
-include ("logs/logs.php");
-require_once '_action/functions.php';
-$crud = new Crud();
+	<?php
+	require_once '_action/functions.php';
+	// require_once 'cabecalho.php';
+	include ("conecta/conexao.php");
+	$crud = new Crud();
 
 
-if(isset($_POST['submit'])){
-	if (isset($_POST['data'])) {
-		$data = $_POST['data'];
-		
-	}else{
-		$data = date_create()->format('Y-m-d');
-	}
-	$estado = filter_input(INPUT_POST, 'status', FILTER_SANITIZE_STRING);
-	$id_assist = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
-	$id_prof = filter_input(INPUT_POST, 'profissionais', FILTER_SANITIZE_NUMBER_INT);
-	$id_serv = filter_input(INPUT_POST, 'servicos', FILTER_SANITIZE_NUMBER_INT);
-	$tipo = filter_input(INPUT_POST, 'convenio', FILTER_SANITIZE_STRING);
-
-	//Se ele estiver sendo desligado vai mudar o seu ativo de "sim" para "não" fazendo o mesmo sumir da agenda;
-	if($estado=="Alta")
-	{
-		$ativo="N";
-		include ("header/agenda-up-desliga.php");
-	}
-
-
-	$data = [
-		"id_assist" => $id_assist,
-		"data" => $data,
-		"estado" => $estado,
-		"id_prof" => $id_prof,
-		"id_serv" => $id_serv,
-		"tipo" => $tipo
-	];
-
-	$crud->create("atendimentos", $data);
-
-
-	if ($estado == "Falta") {
-		$faltas = $crud->verificarFaltas($id_assist);
-		if($faltas >= 3) {
-			echo "O paciente está com muitas faltas.";
+		$d = date("N");
+		if (isset($_POST['data'])) {
+			if (strtotime($data = $_POST['data']) !== false) {
+				$data = $_POST['data'];
+			} else {
+				$data = date_create()->format('Y-m-d');
+			}
 		}else{
-			echo "sem faltas";
+			$data = date_create()->format('Y-m-d');
 		}
-	}
+		$estado = filter_input(INPUT_POST, 'status', FILTER_SANITIZE_STRING);
+		$id_assist = filter_input(INPUT_POST, 'id_assist', FILTER_SANITIZE_NUMBER_INT);
+		$id_prof = filter_input(INPUT_POST, 'id_prof', FILTER_SANITIZE_NUMBER_INT);
+		$id_serv = filter_input(INPUT_POST, 'id_serv', FILTER_SANITIZE_NUMBER_INT);
+		$tipo = filter_input(INPUT_POST, 'conv', FILTER_SANITIZE_STRING);
+		if (isset($_POST['id_agenda'])) {
+			$id_agenda = filter_input(INPUT_POST, 'id_agenda', FILTER_SANITIZE_NUMBER_INT);
+
+		}else{
+			$id_agenda = null;
+		}
+	//Se ele estiver sendo desligado vai mudar o seu ativo de "sim" para "não" fazendo o mesmo sumir da agenda;
+		if($estado=="Desligado" OR $estado=="Alta")
+		{
+			$ativo="N";
+			include ("header/agenda-up-desliga.php");
+		}
+
+		$data = [
+			"id_assist" => $id_assist,
+			"data" => $data,
+			"estado" => $estado,
+			"id_prof" => $id_prof,
+			"id_serv" => $id_serv,
+			"tipo" => $tipo,
+			"obs" => $id_agenda
+		];
+		$crud->create("atendimentos", $data);
+
+		if ($estado == "Falta") {
+			$faltas = $crud->verificarFaltas($id_assist);
+			if($faltas >= 3) {
+				$registros = $crud->read("assistidos", $id_assist, "id_assist");
+				$mensagem = 'O assistido <strong>' . $registros["nome"] . '</strong> teve três ou mais faltas nos últimos 5 atendimentos.';
+				return $mensagem;
+			}else{
+				header ("Refresh: 0, relatorio"); die; exit;
+			}
+		}
+
+	?>
 
 
-
-	// try{
-	// 	$insert = "INSERT into `atendimentos` (id_assist,data,estado,id_prof,id_serv,tipo)
-	// 	VALUES (:id_assist, :data, :estado, :id_prof, :id_serv, :tipo)";
-	// 	$result = $conexao->prepare($insert);
-	// 	$result->bindParam(':id_assist', $id_assist, PDO::PARAM_INT);
-	// 	$result->bindParam(':data', $data, PDO::PARAM_STR);
-	// 	$result->bindParam(':estado', $estado, PDO::PARAM_STR);
-	// 	$result->bindParam(':id_prof', $id_prof, PDO::PARAM_STR);
-	// 	$result->bindParam(':id_serv', $id_serv, PDO::PARAM_STR);
-	// 	$result->bindParam(':tipo', $tipo, PDO::PARAM_STR);
-	// 	$result->execute();
-	// 	registraLog($id_assist, $id_serv, $id_prof, $estado, $_SESSION['nome'] );
-	// 	header ("Location: relatorio");
-	// }catch(PDOException $e){echo $e;}
-}
-?>
+			</body>
+			</html>
